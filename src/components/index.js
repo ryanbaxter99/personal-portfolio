665eebeac749bedf764d531f86e6e8cb04e6051e
ryanbaxter99 <rybaxter@umich.edu>
2023-04-22 21:01:30 -0400

    Removed Computer.jsx component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, BallCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
@@ -20,7 +20,6 @@ export {
   Contact,
   EarthCanvas, 
   BallCanvas, 
-  ComputersCanvas, 
   StarsCanvas,
   WelcomePage
 }
